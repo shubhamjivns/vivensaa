07d5e231887b501d0f904a0fc0ae21b9d124621f
Shubhamjivns <mishra.darksoul@gmail.com>
2025-11-01 20:58:05 +0530

    Fixed slider

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),],
+  base:'/vivensaa/',
+  
+  plugins:[tailwindcss(),],
 })
